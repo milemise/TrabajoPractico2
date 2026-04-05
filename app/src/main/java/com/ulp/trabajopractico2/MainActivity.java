b54b07098b0bc8412eebea81ab184bbe0901d5c2
milemise <milivicmp@gmail.com>
2026-04-05 12:15:31 -0300

    TP2

---
+++ app/src/main/java/com/ulp/trabajopractico2/MainActivity.java
@@ -0,0 +1,53 @@
+package com.ulp.trabajopractico2;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private UnlockReceiver unlockReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        unlockReceiver = new UnlockReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_USER_PRESENT);
+        registerReceiver(unlockReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(unlockReceiver);
+    }
+
+    private class UnlockReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_USER_PRESENT.equals(intent.getAction())) {
+
+                Toast.makeText(context, "Pantalla desbloqueada detectada", Toast.LENGTH_SHORT).show();
+
+                Log.d("UnlockReceiver", "Evento de desbloqueo detectado");
+
+                Intent intentLlamada = new Intent(Intent.ACTION_DIAL);
+                intentLlamada.setData(Uri.parse("tel:2657234567"));
+                intentLlamada.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intentLlamada);
+            }
+        }
+    }
+}
